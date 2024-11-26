8efb9e36a7d3940c07636c1c6618d14cd9f25113
Prashant <pm21641@gmail.com>
2024-11-27 00:40:50 +0530

    auth bug fix issues

--- src/shared/apis/hospitalCard.js
+++ src/shared/apis/hospitalCard.js
@@ -1,11 +1,7 @@
 import axiosInstance from "./axiosInstance";
 
-export const getHospitalList=async (authToken) => {
-    const response = await axiosInstance.get(`apiV1/hospital-home-slider/`, {
-      headers: {
-        Authorization: `JWT ${authToken}`,
-      },
-    });
-    console.log("the hospital list sre as follos",response.data)
-    return response.data;
-  };
+export const getHospitalList = async () => {
+  const response = await axiosInstance.get(`apiV1/hospital-home-slider/`);
+  console.log("the hospital list sre as follos", response.data);
+  return response.data;
+};
